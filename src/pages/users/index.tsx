25055036ce25cd5edaed0b0b5a081c3dc7ae258f
Italo Carlo <italocarlo@nti.ufal.br>
2021-05-05 20:20:02 -0300

    react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,8 +11,11 @@ import { Text,
          Tbody, 
          Tr, 
          Td, 
-         useBreakpointValue
+         useBreakpointValue,
+         Spinner
 } from "@chakra-ui/react";
+
+import { useQuery } from 'react-query';
 
 import { Header } from "../../components/Header/";
 import { Sidebar } from "../../components/Sidebar/";
@@ -23,16 +26,17 @@ import Link from "next/link";
 import { useEffect } from "react";
 
 export default function UserList(){
+  
+  const { data, isLoading, error }= useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');      
+    const data = await response.json();
+    return data;
+  });
+  
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
-  });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data));
-  },[]);
+  });  
   
   return (
     <Box>
@@ -66,7 +70,17 @@ export default function UserList(){
           
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
+         { isLoading ? (
+           <Flex justify="center">
+             <Spinner />
+           </Flex>
+         ): error ? (
+          <Flex justify="center">
+            Falha ao obter dados
+          </Flex>
+         ): (
+           <>
+            <Table colorScheme="whiteAlpha">
                 <Thead>
                   <Th px={["4","4","6"]} color="gray.300" width="8">
                     <Checkbox colorScheme="pink"></Checkbox>
@@ -104,6 +118,8 @@ export default function UserList(){
             </Table>
 
             <Pagination />
+           </>
+         )}
         </Box>
       </Flex>
     </Box>
