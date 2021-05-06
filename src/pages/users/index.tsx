52247ecc7612c319993140d0e9b0a7290a199744
Italo Carlo <italocarlo@nti.ufal.br>
2021-05-05 22:27:59 -0300

    Hook user list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,35 +15,18 @@ import { Text,
          Spinner
 } from "@chakra-ui/react";
 
-import { useQuery } from 'react-query';
+import Link from "next/link";
 
 import { Header } from "../../components/Header/";
 import { Sidebar } from "../../components/Sidebar/";
 import { Pagination } from "../../components/Pagination/";
 
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import Link from "next/link";
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList(){
   
-  const { data, isLoading, isFetching , error }= useQuery('users', async () => {
-    const { data } = await api.get('/users');          
-    const users = data.users.map( user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR',{
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    });
-    return users;
-  });
-  
+  const { data, isLoading, isFetching , error }= useUsers();
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
